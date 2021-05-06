10b8b689d47b81450eabf747a329cf3643381931
YanZhenJingFan <13142186759@163.com>
2021-05-07 01:13:50 +0800

    fix(): 消息内容和记录表放到mq模块中初始化

---
+++ src/main/resources/db/migration/V1_1__Init.sql
@@ -0,0 +1,21 @@
+#下面的是例子中的表
+CREATE TABLE `t_order`
+(
+    id          BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    create_time DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    edit_time   DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    user_id     BIGINT UNSIGNED NOT NULL COMMENT '用户ID',
+    order_id    VARCHAR(64)     NOT NULL COMMENT '订单ID',
+    amount      DECIMAL(12, 2)  NOT NULL DEFAULT 0 COMMENT '订单金额',
+    INDEX idx_user_id (user_id),
+    UNIQUE uniq_order_id (order_id)
+) COMMENT '订单表';
+
+CREATE TABLE `t_user` (
+  `user_id` varchar(255) NOT NULL DEFAULT '' COMMENT '用户id',
+  `username` varchar(255) DEFAULT '' COMMENT '用户名',
+  `tenant_id` varchar(255) DEFAULT '' COMMENT '租户id',
+  `sex` varchar(255) DEFAULT '' COMMENT '性别（1：男，2：女）',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
