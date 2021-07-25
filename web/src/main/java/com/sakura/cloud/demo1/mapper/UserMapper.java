b509dd68875b618b60b96df59e9be9e09f77f0c1
yangfan <13142186759@163.com>
2021-07-25 17:53:00 +0800

    style(web): 去掉多余的import

--- web/src/main/java/com/sakura/cloud/demo1/mapper/UserMapper.java
+++ web/src/main/java/com/sakura/cloud/demo1/mapper/UserMapper.java
@@ -4,12 +4,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.sakura.cloud.demo1.entity.UserPO;
-import com.sakura.cloud.demo1.excel.UserExcel;
 import com.sakura.cloud.demo1.vo.UserVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 
 @Mapper
