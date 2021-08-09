b0bb036a32f9aa536e970f4e40cac8983160d25f
yangfan <13142186759@163.com>
2021-08-09 20:37:42 +0800

    style():

--- web/src/main/java/com/sakura/cloud/WebApplication.java
+++ web/src/main/java/com/sakura/cloud/WebApplication.java
@@ -2,7 +2,6 @@ package com.sakura.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //开启feign
