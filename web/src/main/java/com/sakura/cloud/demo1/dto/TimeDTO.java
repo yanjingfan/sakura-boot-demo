8250e32e881b43ae2e5cf85c72c6fdec807b2fbc
yangfan <13142186759@163.com>
2022-02-28 10:32:27 +0800

    feat(web): 后台传前台时间，添加fastjson方式转换

--- web/src/main/java/com/sakura/cloud/demo1/dto/TimeDTO.java
+++ web/src/main/java/com/sakura/cloud/demo1/dto/TimeDTO.java
@@ -1,12 +1,12 @@
 package com.sakura.cloud.demo1.dto;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 /**
  * @auther yangfan
@@ -18,10 +18,20 @@ import java.util.Date;
 public class TimeDTO {
 
     @ApiModelProperty("开始时间，格式为yyyy-MM-dd HH:mm:ss")
-    //传到前台的时间格式
+    /**
+     * 传到前台的时间格式，使用Jackson
+     */
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     //传到后台的时间格式
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startTime;
+
+    /**
+     * 传到前台的时间格式，使用fastjson
+     */
+    @JSONField(format="yyyy-MM-dd HH:mm:ss")
+    //传到后台的时间格式
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime endTime;
 
 }
