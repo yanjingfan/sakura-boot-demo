f0e2089351ad18ed90b89b3c009d92691c7cd34c
yangfan <13142186759@163.com>
2022-04-15 11:01:58 +0800

    docs(web)：注释添加时间格式统一处理类后端路径

--- web/src/main/java/com/sakura/cloud/demo1/controller/TimeController.java
+++ web/src/main/java/com/sakura/cloud/demo1/controller/TimeController.java
@@ -2,7 +2,6 @@ package com.sakura.cloud.demo1.controller;
 
 import com.sakura.cloud.demo1.dto.TimeDTO;
 import com.sakura.common.config.date.DateHandlerConfig;
-import com.sakura.common.config.date.GlobalTimeHandler;
 import com.sakura.common.result.CommonResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -21,9 +20,7 @@ import java.util.Date;
  * sakura-boot组件中common模块，处理了LocalDateTime、LocalDate、Date、LocalTime类型的参数，无
  * 需再添加@DateTimeFormat和@JsonFormat/@JSONField，就可以传递时间参数
  *
- * {@link DateHandlerConfig}
- * {@link GlobalTimeHandler}
- *
+ * 时间格式统一处理：{@link DateHandlerConfig}
  *
  */
 @RestController
