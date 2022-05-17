3064406a25b575016de6b3c7c69e9651d6d67b4b
yangfan <13142186759@163.com>
2022-05-17 16:28:51 +0800

    feat(): 添加gitlab自动化脚本，使可以在gitlab上手动打包部署

---
+++ web/run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# 定义应用组名
+group_name='sakura-boot'
+# 定义应用名称
+app_name='web'
+# 定义应用版本
+app_version='0.0.1'
+# 定义应用环境
+#profile_active='prod'
+echo '----copy jar----'
+docker stop ${app_name}
+echo '----stop container----'
+docker rm ${app_name}
+echo '----rm container----'
+docker rmi ${group_name}/${app_name}:${app_version}
+echo '----rm image----'
+# 打包编译docker镜像
+docker build -t ${group_name}/${app_name}:${app_version} .
+echo '----build image----'
+docker run -p 8080:8080 --name ${app_name} \
+-e TZ="Asia/Shanghai" \
+-v /etc/localtime:/etc/localtime \
+-v /mydata/app/${app_name}/logs:/var/logs \
+-d ${group_name}/${app_name}:${app_version}
+echo '----start container----'
