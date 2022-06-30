bf5b546a5cbf40a150aa7c036e5ac6654d030deb
yangfan <13142186759@163.com>
2022-06-30 15:36:20 +0800

    docs(): 初始化表添加undo_log

--- seata/docs/account.sql
+++ seata/docs/account.sql
@@ -9,3 +9,17 @@ CREATE TABLE `account` (
 
 INSERT INTO `seat-account`.`account` (`id`, `user_id`, `total`, `used`, `residue`) VALUES ('1', '1', '1000', '0', '1000');
 
+CREATE TABLE `undo_log` (
+    `id`            BIGINT(20)   NOT NULL AUTO_INCREMENT,
+    `branch_id`     BIGINT(20)   NOT NULL,
+    `xid`           VARCHAR(100) NOT NULL,
+    `context`       VARCHAR(128) NOT NULL,
+    `rollback_info` LONGBLOB     NOT NULL,
+    `log_status`    INT(11)      NOT NULL,
+    `log_created`   DATETIME     NOT NULL,
+    `log_modified`  DATETIME     NOT NULL,
+    `ext`           VARCHAR(100) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4;
+
--- seata/docs/order.sql
+++ seata/docs/order.sql
@@ -8,3 +8,17 @@ CREATE TABLE `order` (
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 ALTER TABLE `order` ADD COLUMN `status` int(1) DEFAULT NULL COMMENT '订单状态：0：创建中；1：已完结' AFTER `money` ;
+
+CREATE TABLE `undo_log` (
+    `id`            BIGINT(20)   NOT NULL AUTO_INCREMENT,
+    `branch_id`     BIGINT(20)   NOT NULL,
+    `xid`           VARCHAR(100) NOT NULL,
+    `context`       VARCHAR(128) NOT NULL,
+    `rollback_info` LONGBLOB     NOT NULL,
+    `log_status`    INT(11)      NOT NULL,
+    `log_created`   DATETIME     NOT NULL,
+    `log_modified`  DATETIME     NOT NULL,
+    `ext`           VARCHAR(100) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4;
