2a08506cdf0c88eabdcd6c391a8cec22285e2950
yangfan <13142186759@163.com>
2022-09-02 15:00:01 +0800

    feat(): 添加白名单配置

--- sa-token/sa-token-gateway/src/main/java/com/sakura/cloud/sa/gateway/config/SaTokenConfig.java
+++ sa-token/sa-token-gateway/src/main/java/com/sakura/cloud/sa/gateway/config/SaTokenConfig.java
@@ -1,17 +1,19 @@
 package com.sakura.cloud.sa.gateway.config;
 
-import cn.dev33.satoken.filter.SaFilterErrorStrategy;
 import cn.dev33.satoken.reactor.context.SaReactorSyncHolder;
 import cn.dev33.satoken.reactor.filter.SaReactorFilter;
-import cn.dev33.satoken.router.SaRouter;
-import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.server.ServerWebExchange;
 
 @Configuration
 public class SaTokenConfig {
+
+    @Autowired
+    private IgnoreUrlsConfig ignoreUrlsConfig;
+
     /**
      * 注册Sa-Token全局过滤器
      */
@@ -21,11 +23,11 @@ public class SaTokenConfig {
                 // 拦截地址
                 .addInclude("/**")
                 // 开放地址
-                .addExclude("/favicon.ico")
+                .setExcludeList(ignoreUrlsConfig.getUrls())
                 // 鉴权方法：每次访问进入
                 .setAuth(r -> {
                     // 登录认证：除登录接口都需要认证
-                    SaRouter.match("/**", "/sa-token-auth/user/login", StpUtil::checkLogin);
+//                    SaRouter.match("/**", "/sa-token-auth/user/login", StpUtil::checkLogin);
                     // 权限认证：不同接口访问权限不同
 //                    SaRouter.match("/api/test/hello", () -> StpUtil.checkPermission("api:test:hello"));
 //                    SaRouter.match("/api/user/info", () -> StpUtil.checkPermission("api:user:info"));
@@ -35,7 +37,7 @@ public class SaTokenConfig {
                     //设置错误返回格式为JSON
                     ServerWebExchange context = SaReactorSyncHolder.getContext();
                     context.getResponse().getHeaders().set("Content-Type", "application/json; charset=utf-8");
-                    return SaResult.error(e.getMessage());
+                    return SaResult.error().setCode(401).set("message", e.getMessage());
                 });
     }
 }
