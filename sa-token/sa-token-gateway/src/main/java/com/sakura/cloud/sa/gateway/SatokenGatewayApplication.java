ae9f36c1bac403512dcdbbebb7d61453aedb0bce
yangfan <13142186759@163.com>
2022-09-15 18:14:55 +0800

    fix(sa-token-gateway): 解决启动报错问题

--- sa-token/sa-token-gateway/src/main/java/com/sakura/cloud/sa/gateway/SatokenGatewayApplication.java
+++ sa-token/sa-token-gateway/src/main/java/com/sakura/cloud/sa/gateway/SatokenGatewayApplication.java
@@ -3,7 +3,7 @@ package com.sakura.cloud.sa.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"com.sakura"})//扫描注入的bean
+@SpringBootApplication
 public class SatokenGatewayApplication {
 
     public static void main(String[] args) {
